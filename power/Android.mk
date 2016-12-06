e76d2eb8a919f08fb9fdd1072a54f8d29539d739
Arvin Quilao <arquilao@gmail.com>
2016-12-06 07:59:48 +0000

    msm8916: import CAF Power HAL
    Change-Id: I9ebcb99bec3687e9ccceeec2dc274de1183fb3e2

--- power/Android.mk
+++ power/Android.mk
@@ -12,13 +12,25 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-LOCAL_PATH:= $(call my-dir)
+LOCAL_PATH := $(call my-dir)
+
+# HAL module implemenation stored in
+# hw/<POWERS_HARDWARE_MODULE_ID>.<ro.hardware>.so
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES := power-feature.c
-LOCAL_SHARED_LIBRARIES := liblog libcutils
+LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_SHARED_LIBRARIES := liblog libcutils libdl
+LOCAL_SRC_FILES := power.c \
+                   metadata-parser.c \
+                   utils.c \
+                   list.c \
+                   hint-data.c \
+                   power-8916.c
 
-LOCAL_MODULE := libpower_set_feature_tomato
+ifeq ($(TARGET_USES_INTERACTION_BOOST),true)
+    LOCAL_CFLAGS += -DINTERACTION_BOOST
+endif
+
+LOCAL_MODULE := power.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_TAGS := optional
-
-include $(BUILD_STATIC_LIBRARY)
+include $(BUILD_SHARED_LIBRARY)
