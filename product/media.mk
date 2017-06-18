975d2124cca9f4ae6e3af9ad40d3d6996dec9849
ishantvivek <ishantvivek1311@gmail.com>
2017-06-18 04:02:21 +0000

    tomato: add volte support
    init: Add ims-related daemons
    
    Change-Id: I9a1f6c8b2cb6b89ad1e93236cefbeb4fd2b52f8a
    
    rootdir: init: start IMS on bootup
    
    Change-Id: I1bee1d68f7c9218bbfdf0ab2117a28f16ddad3cb
    
    overlay: Add config flags for VoLTE and VT configurations
    
    In lmr1 config flags for VoLTE and VT are separated
    Adding overlay config flags to enable VoLTE and VT configurations.
    
    Change-Id: I603209cc4cb891c1ef9103958d81ae7d65f0043c
    
    tomato: Update ims,radio properties
    
    Change-Id: I6df843c0247030c14d0bd76ca8dfe2c13f05066a
    
    tomato: Add missing functions and symbols for IMS
    
    Change-Id: I84413beb51f921fffed3b2e8d6f8b5b9c3d9b220
    
    tomato: Add ims blobs
    
    Change-Id: I8b752e4b3a253e560c67b7e2ab32c8975b1ad7b1
    
    tomato: ims: Allow ims to use net_raw capability
    Change-Id: Iec28c66df6ad3db645d66411c097bd65d29c13cb
    
    volte: Added missing Parcel shim for cnd & dpmd
    
    volte: Add _ZNK7android13ICameraClient22getInterfaceDescriptorEv
    
    Signed-off-by: Ritesh Saxena <riteshsax007@gmail.com>
    Signed-off-by: Shekhawat2 <anandsingh215@yahoo.com>

--- product/media.mk
+++ product/media.mk
@@ -1,3 +1,8 @@
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
+
+# Widevine DRM symbol, boringssl-compat subset
+PRODUCT_PACKAGES += \
+    libshim_parcel \
+    libshim_boringssl
