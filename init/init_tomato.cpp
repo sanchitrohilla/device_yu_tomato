52d981a3a8a143d2a36e3a17b9ed17b18a116119
Shekhawat2 <anandsingh215@yahoo.com>
2017-09-15 09:55:43 +0530

    Change DPI
    Change-Id: I1fad9e20358d09a56f5f7456a33966d01b290b0b

--- init/init_tomato.cpp
+++ init/init_tomato.cpp
@@ -39,7 +39,7 @@
 
 using android::base::GetProperty;
 
-static int display_density = 320;
+static int display_density = 230;
 
 static void import_cmdline(const std::string& key,
         const std::string& value, bool for_emulator __attribute__((unused)))
