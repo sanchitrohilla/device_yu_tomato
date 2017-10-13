3a9e04e7aa4014e9ab0d199e4c2e440af79631b8
luca020400 <luca.stefani.ge1@gmail.com>
2017-10-13 08:53:46 +0530

    gps: Add get_process_name shim
    Signed-off-by: Shekhawat2 <anandsingh215@yahoo.com>

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -38,3 +38,11 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 
 include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := get_process_name.c
+LOCAL_MODULE := libshims_get_process_name
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_SHARED_LIBRARY)
--- product/qcom-gps.mk
+++ product/qcom-gps.mk
@@ -1,7 +1,8 @@
 # GPS
 PRODUCT_PACKAGES += \
     android.hardware.gnss@1.0-impl \
-    gps.msm8916
+    gps.msm8916 \
+    libshims_get_process_name
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/flp.conf:system/etc/flp.conf \
