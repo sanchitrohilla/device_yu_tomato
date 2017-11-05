073f651feeba0201a751876c0e5d4e8995ea522c
Shekhawat2 <anandsingh215@yahoo.com>
2017-11-05 10:24:07 +0000

    Add apns-conf.xml

--- product/qcom-radio.mk
+++ product/qcom-radio.mk
@@ -15,3 +15,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/data/netmgr_config.xml:system/etc/data/netmgr_config.xml \
     $(LOCAL_PATH)/configs/data/qmi_config.xml:system/etc/data/qmi_config.xml \
     $(LOCAL_PATH)/configs/data/dsi_config.xml:system/etc/data/dsi_config.xml
+
+# apn
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/apns-conf.xml:system/etc/apns-conf.xml
