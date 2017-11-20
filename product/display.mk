d12fe0af2791a8a3bd3df565868e2793d0d07fba
Shekhawat2 <anandsingh215@yahoo.com>
2017-11-20 12:06:24 +0530

    Add phone-xxhdpi-2048-hwui-memory.mk
    Signed-off-by: Shekhawat2 <anandsingh215@yahoo.com>

--- product/display.mk
+++ product/display.mk
@@ -7,4 +7,4 @@ PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
 # Dalvik
-$(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
+$(call inherit-product, device/yu/tomato/display/phone-xxhdpi-2048-hwui-memory.mk)
