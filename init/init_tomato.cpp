d54d934f3a2e29886b2918372c241b8436f484e4
Shekhawat2 <anandsingh215@yahoo.com>
2017-12-16 01:06:36 +0530

    init: update for 8.1
    Signed-off-by: Shekhawat2 <anandsingh215@yahoo.com>

--- init/init_tomato.cpp
+++ init/init_tomato.cpp
@@ -38,7 +38,8 @@
 #include "init_msm8916.h"
 
 using android::base::GetProperty;
-
+using android::init::property_set;
+using android::init::import_kernel_cmdline;
 static int display_density = 230;
 
 static void import_cmdline(const std::string& key,
