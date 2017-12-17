2b5f3a9d73dcccf762de7ee8c6d97d2a5bdde19a
Shekhawat2 <anandsingh215@yahoo.com>
2017-12-17 03:41:42 +0000

    Configure OMX
    Signed-off-by: Shekhawat2 <anandsingh215@yahoo.com>

--- product/media.mk
+++ product/media.mk
@@ -6,3 +6,25 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libshim_parcel \
     libshim_boringssl
+
+# OMX
+PRODUCT_PACKAGES += \
+    libc2dcolorconvert \
+    libOmxAacEnc \
+    libOmxAmrEnc \
+    libOmxCore \
+    libOmxEvrcEnc \
+    libOmxQcelp13Enc \
+    libOmxSwVencHevc \
+    libOmxVdec \
+    libOmxVenc \
+    libstagefrighthw
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    media.stagefright.audio.sink=280 \
+    vidc.dec.downscalar_width=1920 \
+    vidc.dec.downscalar_height=1088 \
+    vidc.disable.split.mode=1 \
+    vidc.enc.disable_bframes=1 \
+    vidc.enc.disable_pframes=1 \
+    vidc.enc.narrow.searchrange=1
