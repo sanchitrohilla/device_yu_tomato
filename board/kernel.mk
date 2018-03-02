bfec07160b1b9ad92c30a2cf4e1085d13a35edc4
sanchitrohilla <sanchitrohilla64@gmail.com>
2018-03-02 13:38:11 +0000

    kernel properties

--- board/kernel.mk
+++ board/kernel.mk
@@ -5,3 +5,6 @@ BOARD_KERNEL_SEPARATED_DT := true
 LZMA_RAMDISK_TARGETS := recovery
 TARGET_KERNEL_CONFIG := lineageos_tomato_defconfig
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_HEADER_ARCH := arm64
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
