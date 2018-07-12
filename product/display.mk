7b0872d1e602ac69d1a13a325e7f1c56bb21e87f
sanchitrohilla <sanchitrohilla64@gmail.com>
2018-07-12 12:23:24 +0000

    Remove hwui not needed in 8.1

--- product/display.mk
+++ product/display.mk
@@ -6,5 +6,4 @@ TARGET_SCREEN_WIDTH := 720
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
-# Dalvik
-$(call inherit-product, device/yu/tomato/display/phone-xxhdpi-2048-hwui-memory.mk)
+
