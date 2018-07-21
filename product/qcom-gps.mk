2339cecfc207ccfc0d769304df68f33a58192633
sanchitrohilla <sanchitrohilla64@gmail.com>
2018-07-21 11:04:31 +0000

    Gps : edit conf path implementation of loc_api

--- product/qcom-gps.mk
+++ product/qcom-gps.mk
@@ -1,6 +1,5 @@
 # GPS
 PRODUCT_PACKAGES += \
-    gps.msm8916 \
     libcurl \
     libgnss \
     libgnsspps \
@@ -12,12 +11,12 @@ PRODUCT_PACKAGES += \
     android.hardware.gnss@1.0-service-qti
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/gps/flp.conf:system/etc/flp.conf \
-    $(LOCAL_PATH)/gps/gps.conf:system/etc/gps.conf \
-    $(LOCAL_PATH)/gps/xtwifi.conf:system/etc/xtwifi.conf \
-    $(LOCAL_PATH)/gps/lowi.conf:system/etc/lowi.conf \
-    $(LOCAL_PATH)/gps/izat.conf:system/etc/izat.conf \
-    $(LOCAL_PATH)/gps/sap.conf:system/etc/sap.conf
+    $(LOCAL_PATH)/gps/etc/flp.conf:system/etc/flp.conf \
+    $(LOCAL_PATH)/gps/etc/gps.conf:system/etc/gps.conf \
+    $(LOCAL_PATH)/gps/etc/xtwifi.conf:system/etc/xtwifi.conf \
+    $(LOCAL_PATH)/gps/etc/lowi.conf:system/etc/lowi.conf \
+    $(LOCAL_PATH)/gps/etc/izat.conf:system/etc/izat.conf \
+    $(LOCAL_PATH)/gps/etc/sap.conf:system/etc/sap.conf
 
 # Permissions
 PRODUCT_COPY_FILES += \
