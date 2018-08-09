6d64490b91c00c2dd16a92f77963353b4609d3e2
sanchitrohilla <sanchitrohilla64@gmail.com>
2018-08-09 14:06:45 +0000

    Define architecture

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -23,8 +23,20 @@ TARGET_BOARD_INFO_FILE := device/yu/tomato/board-info.txt
 # Inherit from proprietary files
 include vendor/yu/tomato/BoardConfigVendor.mk
 ALLOW_MISSING_DEPENDENCIES =true
-WITH_DEXPREOPT := false
-TARGET_CRYPTFS_HW_PATH := device/yu/tomato/cryptfs_hw
+WITH_DEXPREOPT := true
 
 #Selinux
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+#TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := cortex-a53
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+#TARGET_2ND_CPU_ABI :=
+#TARGET_2ND_CPU_ABI2 :=
+TARGET_2ND_CPU_VARIANT := cortex-a53
